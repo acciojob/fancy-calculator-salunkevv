4a60a6c3996a288f3977a0f982ade57e5f4d5fae
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-16 09:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
+value1 = value1*2;
+let add = Number(value1)+ Number(value2);
+alert(`The final result is: ${add}`);
